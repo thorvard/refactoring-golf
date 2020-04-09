eee840e808c62dfd8aaac530c15d4dfd52b1ff92
Angelo Mastropietro <angelo@wallapop.com>
2020-04-09 16:08:48 +0200

    Extract method in test

--- src/payroll/hole04/PayslipTest.java
+++ src/payroll/hole04/PayslipTest.java
@@ -7,34 +7,29 @@ import static org.junit.Assert.assertEquals;
 public class PayslipTest {
     @Test
     public void taxIsZeroIfGrossIsBelowTaxFreeLimit() {
-        Payslip payslip = new Payslip(5000, new TaxCalculator());
-        assertEquals(5000, payslip.getNet(), 1e-6);
+        assertNetGivenGross(5000, 5000);
     }
 
     @Test
     public void taxOnAmountInLowerTaxBracketExcludesTaxFreeLimit() {
-        final Payslip payslip = new Payslip(10000, new TaxCalculator());
-        assertEquals(9500, payslip.getNet(), 1e-6);
-
-        final Payslip payslip2 = new Payslip(20000, new TaxCalculator());
-        assertEquals(18500, payslip2.getNet(), 1e-6);
+        assertNetGivenGross(10000, 9500);
+        assertNetGivenGross(20000, 18500);
     }
 
     @Test
     public void taxOnAmountInMiddleTaxBracketIsSumOfLowerTaxBracketAmountAndAdditionalMiddleTaxBracketAmount() {
-        final Payslip payslip = new Payslip(25000, new TaxCalculator());
-        assertEquals(22500, payslip.getNet(), 1e-6);
-
-        final Payslip payslip2 = new Payslip(40000, new TaxCalculator());
-        assertEquals(34500, payslip2.getNet(), 1e-6);
+        assertNetGivenGross(25000, 22500);
+        assertNetGivenGross(40000, 34500);
     }
 
     @Test
     public void taxOnAmountInUpperTaxBracketIsSumOfLowerTaxBracketAmountAndMiddleTaxBracketAmountAndAdditionalUpperTaxBracketAmount() {
-        final Payslip payslip = new Payslip(50000, new TaxCalculator());
-        assertEquals(40500, payslip.getNet(), 1e-6);
+        assertNetGivenGross(50000, 40500);
+        assertNetGivenGross(60000, 46500);
+    }
 
-        final Payslip payslip2 = new Payslip(60000, new TaxCalculator());
-        assertEquals(46500, payslip2.getNet(), 1e-6);
+    private void assertNetGivenGross(int gross, int expectedNet) {
+        final Payslip payslip = new Payslip(gross, new TaxCalculator());
+        assertEquals(expectedNet, payslip.getNet(), 1e-6);
     }
 }
